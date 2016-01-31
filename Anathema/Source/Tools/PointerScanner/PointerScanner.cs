a83c083be306074002fb2a78fec7eea2bd960e0a
Zachary Canann <zacharycanann@gmail.com>
2016-01-31 01:01:02 -0800

    Setting up the framework for the pointer scan code
    Former-commit-id: 6e4623401cc9e53e523bbb501fc34c5a4b3cfe81

--- Anathema/Source/Tools/PointerScanner/PointerScanner.cs
+++ Anathema/Source/Tools/PointerScanner/PointerScanner.cs
@@ -2,6 +2,7 @@
 using Binarysharp.MemoryManagement.Memory;
 using Binarysharp.MemoryManagement.Modules;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,33 +11,37 @@ using System.Threading.Tasks;
 namespace Anathema
 {
     /*
-    The algorithm:
+    Trace-Retrace Algorithm:
     0) Potential pre-processing -- no idea how many valid pointers exist in a process, but we may be able to:
         - Store all pointers to use
         - Store all regions that contain a pointer
 
     1) Start with a base address. Convert this to a range that spans 1024 in each direction, add this to the target list
-    REPEAT FOR N LEVELS:
-    2) Search for all pointer values that fall in in the target list
-    3) Convert these pointers to spanning regions, and add them to the target list, clearing the old list
+    2) REPEAT FOR N LEVELS:
+        - Search for all pointer values that fall in in the target list
+        - Convert these pointers to spanning regions, and add them to the target list, clearing the old list
 
-    5) Retrace pointers. We lose the offsets with this algorithm and need to recover them, but it should be fast, right?
-        
+    3) Retrace pointers. We will not trace pointers with invalid bases. Loop from last level to first level:
+        - Compare pointer to all pointers in the previous level. Store offsets from current level to all pointers in previous level.
     */
+
     class PointerScanner : IPointerScannerModel
     {
         private MemorySharp MemoryEditor;
 
         public event PointerScannerEventHandler EventUpdateProcessTitle;
 
+        private Int32 MaxPointerLevel;
         private List<RemoteModule> Modules;
         private List<RemoteRegion> AcceptedBases;
-
+        
+        private ConcurrentDictionary<UInt64, UInt64> PointerPool;
         private List<RemoteRegion> TargetRegions;
-        private Dictionary<Int32, List<SnapshotElement>> Pointers;
+        // private Dictionary<Int32, List<SnapshotElement>> Pointers;
 
         public PointerScanner()
         {
+            PointerPool = new ConcurrentDictionary<UInt64, UInt64>();
             Modules = new List<RemoteModule>();
             AcceptedBases = new List<RemoteRegion>();
             InitializeProcessObserver();
@@ -52,6 +57,40 @@ namespace Anathema
             this.MemoryEditor = MemoryEditor;
         }
 
+        public void SetMaxPointerLevel(Int32 MaxPointerLevel)
+        {
+            this.MaxPointerLevel = MaxPointerLevel;
+        }
+
+        public void BeginPointerScanner()
+        {
+            Initialize();
+            Trace();
+            Retrace();
+        }
+
+        private void Trace()
+        {
+
+        }
+
+        private void Retrace()
+        {
+
+        }
+
+        private void Initialize()
+        {
+            BuildPointerPool();
+            CollectModules();
+            SetAcceptedBases();
+        }
+        
+        private void BuildPointerPool()
+        {
+            PointerPool.Clear();
+        }
+
         private void CollectModules()
         {
             Modules = MemoryEditor.Modules.RemoteModules.ToList();
@@ -64,11 +103,6 @@ namespace Anathema
 
             // Gather regions from every module as valid base addresses
             Modules.ForEach(x => AcceptedBases.Add(new RemoteRegion(MemoryEditor, x.BaseAddress, x.Size)));
-        }
-
-        public void BeginPointerScanner()
-        {
-            RemoteModule k = MemoryEditor.Modules.MainModule;
         }
 
     } // End class
