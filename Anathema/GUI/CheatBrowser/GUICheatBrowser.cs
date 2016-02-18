00f6d9cbf7dec5af0dc2a52203795978aa17d202
Zachary Canann <zacharycanann@gmail.com>
2016-02-17 23:33:45 -0800

    Restored browsing and downloading functionality
    Former-commit-id: 9e69d808b1b97183132dd792e60bdb2a3fd602ab

--- Anathema/GUI/CheatBrowser/GUICheatBrowser.cs
+++ Anathema/GUI/CheatBrowser/GUICheatBrowser.cs
@@ -47,38 +47,79 @@ namespace Anathema
             Browser.Navigate(AnathemaCheatBrowseURL);
             Browser.Dock = DockStyle.Fill;
             ContentPanel.Controls.Add(Browser);
-        }
 
-        private void LoadUrl(String URL)
-        {
-            // if (!Uri.IsWellFormedUriString(URL, UriKind.RelativeOrAbsolute))
-            //     return;
-
-            // Browser.Load(URL);
+            LauncherDialog.Download += LauncherDialog_Download;
         }
 
         #region Events
 
+        /// <summary>
+        ///  Handle save file dialog for downloads. Code ripped from the internet, no idea how it works.
+        /// </summary>
+        /// <param name="Sender"></param>
+        /// <param name="E"></param>
+        private void LauncherDialog_Download(Object Sender, LauncherDialogEvent E)
+        {
+            nsILocalFile ObjectTarget = Xpcom.CreateInstance<nsILocalFile>("@mozilla.org/file/local;1");
+
+            using (nsAString nsAString = new nsAString(@Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\temp.tmp"))
+            {
+                ObjectTarget.InitWithPath(nsAString);
+            }
+            
+            Stream SaveStream;
+            SaveFileDialog SaveFileDialog = new SaveFileDialog();
+
+            SaveFileDialog.Filter = "All files (*.*)|*.*";
+            SaveFileDialog.FilterIndex = 2;
+            SaveFileDialog.RestoreDirectory = true;
+            SaveFileDialog.FileName = E.Filename;
+
+            if (SaveFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            if ((SaveStream = SaveFileDialog.OpenFile()) == null)
+                return;
+
+            nsIURI Source = IOService.CreateNsIUri(E.Url);
+            nsIURI Destination = IOService.CreateNsIUri(new Uri(SaveFileDialog.FileName).AbsoluteUri);
+            nsAStringBase StringBase = new nsAString(System.IO.Path.GetFileName(SaveFileDialog.FileName));
+
+            nsIWebBrowserPersist Persist = Xpcom.CreateInstance<nsIWebBrowserPersist>("@mozilla.org/embedding/browser/nsWebBrowserPersist;1");
+            nsIDownloadManager DownloadMan = null;
+            DownloadMan = Xpcom.CreateInstance<nsIDownloadManager>("@mozilla.org/download-manager;1");
+            nsIDownload Download = DownloadMan.AddDownload(0, Source, Destination, StringBase, E.Mime, 0, null, (nsICancelable)Persist, false);
+
+            if (Download != null)
+            {
+                Persist.SetPersistFlagsAttribute(2 | 32 | 16384);
+                Persist.SetProgressListenerAttribute((nsIWebProgressListener)Download);
+                Persist.SaveURI(Source, null, null, null, null, (nsISupports)Destination, null);
+            }
+
+            SaveStream.Close();
+        }
+
         private void HomeButton_Click(Object Sender, EventArgs E)
         {
-            // Browser.Load(AnathemaCheatBrowseURL);
+            Browser.Navigate(AnathemaCheatBrowseURL);
         }
 
         private void UploadButton_Click(Object Sender, EventArgs E)
         {
-            // Browser.Load(AnathemaCheatUploadURL);
+            Browser.Navigate(AnathemaCheatUploadURL);
         }
 
         private void BackButton_Click(Object Sender, EventArgs E)
         {
-            //if (Browser.CanGoBack)
-            //     Browser.Back();
+            if (Browser.CanGoBack)
+                Browser.GoBack();
         }
 
         private void ForwardButton_Click(Object Sender, EventArgs E)
         {
-            //if (Browser.CanGoForward)
-            //    Browser.Forward();
+            if (Browser.CanGoForward)
+                Browser.GoForward();
         }
 
         #endregion
