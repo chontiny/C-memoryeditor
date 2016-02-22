511204f56508f1764f758d02f00353687a38b364
Zachary Canann <zacharycanann@gmail.com>
2016-02-21 19:41:49 -0800

    Adding class for dealing with product registration
    Former-commit-id: f9ef1558de4c9c47df0f06c83f6c19f4a419bb4e

---
+++ Anathema/Source/Registration/RegistrationManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anathema
+{
+    class RegistrationManager
+    {
+        public RegistrationManager()
+        {
+
+        }
+
+    } // End class
+
+} // End namespace
