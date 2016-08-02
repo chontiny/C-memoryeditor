e325419bea02d4f9707f9ed0b6ec1ac4b65a53a3
Zachary Canann <zacharycanann@gmail.com>
2016-08-01 21:39:36 -0700

    Updated gitignore to include untracked mono source files
    Former-commit-id: 022d77bcf1071e1336c37599c560d6ec9e3ddbdb

---
+++ Cecil/symbols/pdb/Microsoft.Cci.Pdb/PdbSource.cs
@@ -0,0 +1,32 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//-----------------------------------------------------------------------------
+using System;
+
+namespace Microsoft.Cci.Pdb
+{
+    public class PdbSource
+    {
+        //public uint index;
+        public string name;
+        public Guid doctype;
+        public Guid language;
+        public Guid vendor;
+
+        public PdbSource(/*uint index, */string name, Guid doctype, Guid language, Guid vendor)
+        {
+            //this.index = index;
+            this.name = name;
+            this.doctype = doctype;
+            this.language = language;
+            this.vendor = vendor;
+        }
+    }
+}
