bc91a3c6589c80a8dbcba72c886ed0957af22dda
Zachary Canann <zacharycanann@gmail.com>
2016-09-17 16:02:05 -0700

    Basic MMVM in place
    Former-commit-id: 13741cc54de5574c51e656006432d3f8579990bb

---
+++ Ana/Source/MVVM/ViewModelLocator.cs
@@ -0,0 +1,38 @@
+﻿namespace Ana.Source.MVVM
+{
+    using Main;
+
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Main view model
+        /// </summary>
+        private static MainViewModel mainViewModel;
+
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            mainViewModel = new MainViewModel();
+        }
+
+        /// <summary>
+        /// Gets the Main property which defines the main view model
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main
+        {
+            get
+            {
+                return mainViewModel;
+            }
+        }
+    }
+    //// End class
+}
+//// End namespace
---
+++ Ana/Source/Main/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿namespace Ana.Source.Main
+{
+    using GalaSoft.MvvmLight;
+    using GalaSoft.MvvmLight.CommandWpf;
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// This class contains properties that the main View can data bind to
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private Int32 exampleValue;
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class
+        /// </summary>
+        public MainViewModel()
+        {
+            this.ShowPopUp = new RelayCommand(() => this.ShowPopUpExecute(), () => true);
+            this.IncrementValue = new RelayCommand(() => this.IncrementValueExecute(), () => true);
+            this.ExampleValue = 0;
+        }
+
+        public ICommand ShowPopUp { get; private set; }
+
+        public ICommand IncrementValue { get; private set; }
+
+        public Int32 ExampleValue
+        {
+            get
+            {
+                return this.exampleValue;
+            }
+
+            set
+            {
+                if (this.exampleValue == value)
+                {
+                    return;
+                }
+
+                this.exampleValue = value;
+                this.RaisePropertyChanged("ExampleValue");
+            }
+        }
+
+        private void ShowPopUpExecute()
+        {
+            MessageBox.Show("Hello World!");
+        }
+
+        private void IncrementValueExecute()
+        {
+            this.ExampleValue += 1;
+        }
+    }
+    //// End class
+}
+//// End namespace
