0725e2ea5e17ef87431c2c41cd99b3df4fb2ed36
Zachary Canann <zacharycanann@gmail.com>
2016-10-11 01:16:07 -0700

    Adding double left/right arrow art assets, paging of results is functional
    Former-commit-id: a43f2db9bfe59b24b29d18b4f592ce9d8b5d7403

--- Ana/Source/Results/ScanResultsViewModel.cs
+++ Ana/Source/Results/ScanResultsViewModel.cs
@@ -9,6 +9,7 @@
     using System.Threading;
     using System.Windows.Input;
     using Utils.Validation;
+
     /// <summary>
     /// View model for the Process Selector
     /// </summary>
@@ -20,14 +21,19 @@
         public const String ToolContentId = nameof(ScanResultsViewModel);
 
         /// <summary>
+        /// The number of elements to display on each page
+        /// </summary>
+        private const Int32 PageSize = 128;
+
+        /// <summary>
         /// Singleton instance of the <see cref="ScanResultsViewModel" /> class
         /// </summary>
         private static Lazy<ScanResultsViewModel> scanResultsViewModelInstance = new Lazy<ScanResultsViewModel>(
                 () => { return new ScanResultsViewModel(); },
                 LazyThreadSafetyMode.PublicationOnly);
 
+        private UInt64 currentPage;
         private UInt64 addressCount;
-        private List<SnapshotElement> activeViewableElements;
         private IEnumerable<SnapshotElement> addresses;
 
         /// <summary>
@@ -36,39 +42,71 @@
         private ScanResultsViewModel() : base("Scan Results")
         {
             this.ContentId = ToolContentId;
+            this.FirstPageCommand = new RelayCommand(() => this.FirstPage(), () => true);
+            this.LastPageCommand = new RelayCommand(() => this.LastPage(), () => true);
+            this.PreviousPageCommand = new RelayCommand(() => this.PreviousPage(), () => true);
+            this.NextPageCommand = new RelayCommand(() => this.NextPage(), () => true);
             this.AddAddressCommand = new RelayCommand<Object>((address) => this.AddAddress(address), (address) => true);
-            this.activeViewableElements = new List<SnapshotElement>();
             this.IsVisible = true;
-
-            // Temp debug
-            SnapshotRegion<Null> k = new SnapshotRegion<Null>(new IntPtr(0x6666), 420);
-            this.addresses = new List<SnapshotElement>(new SnapshotElement[] { new SnapshotElement<Null>(k), new SnapshotElement<Null>(k),
-                new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k),
-                new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k),
-                new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k),
-                new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k),
-                new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k), new SnapshotElement<Null>(k),
-                new SnapshotElement<Null>(k) });
-            this.RaisePropertyChanged(nameof(this.Addresses));
+            this.addresses = new List<SnapshotElement>();
 
             SnapshotManager.GetInstance().Subscribe(this);
             MainViewModel.GetInstance().Subscribe(this);
         }
 
         /// <summary>
-        /// Gets the command to scroll down the view
+        /// Gets the command to go to the first page
         /// </summary>
-        public ICommand ScrollDownViewCommand { get; private set; }
+        public ICommand FirstPageCommand { get; private set; }
 
         /// <summary>
-        /// Gets the command to scroll up the view
+        /// Gets the command to go to the last page
         /// </summary>
-        public ICommand ScrollUpViewCommand { get; private set; }
+        public ICommand LastPageCommand { get; private set; }
+
+        /// <summary>
+        /// Gets the command to go to the previous page
+        /// </summary>
+        public ICommand PreviousPageCommand { get; private set; }
+
+        /// <summary>
+        /// Gets the command to go to the next page
+        /// </summary>
+        public ICommand NextPageCommand { get; private set; }
 
         /// <summary>
         /// Gets the command to select a target process
         /// </summary>
         public ICommand AddAddressCommand { get; private set; }
+
+        /// <summary>
+        /// The total number of addresses found
+        /// </summary>
+        public UInt64 CurrentPage
+        {
+            get
+            {
+                return this.currentPage;
+            }
+
+            set
+            {
+                this.currentPage = value;
+                this.LoadResults();
+                this.RaisePropertyChanged(nameof(this.CurrentPage));
+            }
+        }
+
+        /// <summary>
+        /// The total number of addresses found
+        /// </summary>
+        public UInt64 PageCount
+        {
+            get
+            {
+                return this.ResultCount / ScanResultsViewModel.PageSize;
+            }
+        }
 
         /// <summary>
         /// The the size (in B, KB, MB, GB, TB, etc) of the results found
@@ -95,6 +133,7 @@
                 this.addressCount = value;
                 this.RaisePropertyChanged(nameof(this.ResultCount));
                 this.RaisePropertyChanged(nameof(this.ResultSize));
+                this.RaisePropertyChanged(nameof(this.PageCount));
             }
         }
 
@@ -105,7 +144,7 @@
         {
             get
             {
-                return addresses;
+                return this.addresses;
             }
         }
 
@@ -125,6 +164,63 @@
         public void Update(Snapshot snapshot)
         {
             this.ResultCount = snapshot == null ? 0 : snapshot.GetElementCount();
+            this.CurrentPage = 0;
+        }
+
+        private void LoadResults()
+        {
+            Snapshot snapshot = SnapshotManager.GetInstance().GetActiveSnapshot();
+
+            if (snapshot == null)
+            {
+                return;
+            }
+
+            List<SnapshotElement> newAddresses = new List<SnapshotElement>();
+            UInt64 startIndex = Math.Min(PageSize * CurrentPage, snapshot.GetElementCount());
+            UInt64 endIndex = Math.Min(PageSize * CurrentPage + PageSize, snapshot.GetElementCount());
+
+            for (UInt64 Index = startIndex; Index < endIndex; Index++)
+            {
+                SnapshotRegion<Null> region = new SnapshotRegion<Null>(snapshot[(Int32)Index].BaseAddress, sizeof(UInt64));
+                newAddresses.Add(new SnapshotElement<Null>(region));
+            }
+
+            // Temp debug
+            this.addresses = newAddresses;
+            this.RaisePropertyChanged(nameof(this.Addresses));
+        }
+
+        /// <summary>
+        /// Goes to the first page of results
+        /// </summary>
+        private void FirstPage()
+        {
+            CurrentPage = 0;
+        }
+
+        /// <summary>
+        /// Goes to the last page of results
+        /// </summary>
+        private void LastPage()
+        {
+            CurrentPage = PageCount;
+        }
+
+        /// <summary>
+        /// Goes to the previous page of results
+        /// </summary>
+        private void PreviousPage()
+        {
+            CurrentPage = Math.Max(CurrentPage - 1, 0);
+        }
+
+        /// <summary>
+        /// Goes to the next page of results
+        /// </summary>
+        private void NextPage()
+        {
+            CurrentPage = Math.Min(CurrentPage + 1, PageCount);
         }
 
         /// <summary>
