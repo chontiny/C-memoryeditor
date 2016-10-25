7a7f0610241174997328665fda6f71df44b918f6
Zachary Canann <zacharycanann@gmail.com>
2016-10-24 21:12:04 -0700

    Removing geckoFX dependency and switching to built in web browser. Sucks because its IE under the hood, better because it significantly reduces binary size
    Former-commit-id: 732c3c39ddaac788f93fb680466fdec946179f7f

--- Ana/Source/CheatBrowser/CheatBrowserViewModel.cs
+++ Ana/Source/CheatBrowser/CheatBrowserViewModel.cs
@@ -1,15 +1,11 @@
 ﻿namespace Ana.Source.CheatBrowser
 {
     using Docking;
-    using Engine;
-    using Gecko;
     using Main;
-    using Microsoft.Win32;
     using Mvvm.Command;
     using System;
-    using System.IO;
-    using System.Reflection;
     using System.Threading;
+    using System.Windows.Controls;
     using System.Windows.Input;
 
     /// <summary>
@@ -47,19 +43,8 @@
             this.ContentId = CheatBrowserViewModel.ToolContentId;
 
             // Note: Cannot be async, navigation must take place on the same thread as GUI
-            this.NavigateHomeCommand = new RelayCommand<GeckoWebBrowser>((browser) => this.NavigateHome(browser), (browser) => true);
+            this.NavigateHomeCommand = new RelayCommand<WebBrowser>((browser) => this.NavigateHome(browser), (browser) => true);
 
-            // Initialize engine for Gecko Fx web browser
-            if (EngineCore.GetInstance().OperatingSystemAdapter.IsAnathena32Bit())
-            {
-                Xpcom.Initialize(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "xulrunner-32"));
-            }
-            else if (EngineCore.GetInstance().OperatingSystemAdapter.IsAnathena64Bit())
-            {
-                Xpcom.Initialize(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "xulrunner-64"));
-            }
-
-            LauncherDialog.Download += this.LauncherDialogDownload;
             MainViewModel.GetInstance().Subscribe(this);
         }
 
@@ -81,67 +66,9 @@
         /// Navigates home in the browser
         /// </summary>
         /// <param name="browser">The Gecko Fx web browser</param>
-        private void NavigateHome(GeckoWebBrowser browser)
+        private void NavigateHome(WebBrowser browser)
         {
             browser.Navigate(HomeUrl);
-        }
-
-        /// <summary>
-        /// Handle save file dialog for downloads. Code ripped from the internet, no idea how it works
-        /// </summary>
-        /// <param name="sender">The event sender</param>
-        /// <param name="e">The event parameters</param>
-        private void LauncherDialogDownload(Object sender, LauncherDialogEvent e)
-        {
-            nsILocalFile objectTarget = Xpcom.CreateInstance<nsILocalFile>("@mozilla.org/file/local;1");
-            Stream saveStream;
-
-            String fileName;
-            String fileDirectory;
-
-            using (nsAString nsaString = new nsAString(@Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\temp.tmp"))
-            {
-                objectTarget.InitWithPath(nsaString);
-            }
-
-            // Allow user to select the save location
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = ExtensionFilter;
-            saveFileDialog.FilterIndex = 2;
-            saveFileDialog.RestoreDirectory = true;
-            saveFileDialog.FileName = e.Filename;
-
-            if (saveFileDialog.ShowDialog() != true)
-            {
-                return;
-            }
-
-            fileDirectory = Path.GetDirectoryName(saveFileDialog.FileName);
-            fileName = Path.GetFileName(saveFileDialog.FileName);
-
-            if ((saveStream = File.OpenWrite(Path.Combine(fileDirectory, fileName))) == null)
-            {
-                return;
-            }
-
-            nsIURI source = IOService.CreateNsIUri(e.Url);
-            nsIURI destination = IOService.CreateNsIUri(new Uri(Path.Combine(fileDirectory, fileName)).AbsoluteUri);
-            nsAStringBase stringBase = new nsAString(Path.GetFileName(fileName));
-
-            nsIWebBrowserPersist persist = Xpcom.CreateInstance<nsIWebBrowserPersist>("@mozilla.org/embedding/browser/nsWebBrowserPersist;1");
-            nsIDownloadManager downloadMan = null;
-            downloadMan = Xpcom.CreateInstance<nsIDownloadManager>("@mozilla.org/download-manager;1");
-            nsIDownload download = downloadMan.AddDownload(0, source, destination, stringBase, e.Mime, 0, null, persist, false);
-
-            if (download != null)
-            {
-                persist.SetPersistFlagsAttribute(2 | 32 | 16384);
-                persist.SetProgressListenerAttribute(download);
-                persist.SaveURI(source, null, null, null, null, (nsISupports)destination, null);
-            }
-
-            saveStream.Flush();
-            saveStream.Close();
         }
     }
     //// End class
