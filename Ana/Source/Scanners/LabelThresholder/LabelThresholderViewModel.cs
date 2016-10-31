150d229b0f4e2e6d8959dddd1b0350a4a5145c0b
Zachary Canann <zacharycanann@gmail.com>
2016-10-30 22:03:56 -0700

    Switched to wpftoolkit range slider
    Former-commit-id: 7a7f226651b147df023f2b8818e823554de492ad

--- Ana/Source/Scanners/LabelThresholder/LabelThresholderViewModel.cs
+++ Ana/Source/Scanners/LabelThresholder/LabelThresholderViewModel.cs
@@ -22,14 +22,82 @@
                 () => { return new LabelThresholderViewModel(); },
                 LazyThreadSafetyMode.ExecutionAndPublication);
 
+        private Double lowerValue;
+
+        private Double higherValue;
+
+        private Double minimumValue;
+
+        private Double maximumValue;
+
         /// <summary>
         /// Prevents a default instance of the <see cref="LabelThresholderViewModel" /> class from being created
         /// </summary>
         private LabelThresholderViewModel() : base("Label Thresholder")
         {
             this.ContentId = LabelThresholderViewModel.ToolContentId;
+            this.LowerValue = 0;
+            this.MinimumValue = 0;
+            this.HigherValue = 100;
+            this.MaximumValue = 100;
 
             MainViewModel.GetInstance().Subscribe(this);
+        }
+
+        public Double LowerValue
+        {
+            get
+            {
+                return this.lowerValue;
+            }
+
+            set
+            {
+                this.lowerValue = value;
+                this.RaisePropertyChanged(nameof(this.LowerValue));
+            }
+        }
+
+        public Double HigherValue
+        {
+            get
+            {
+                return this.higherValue;
+            }
+
+            set
+            {
+                this.higherValue = value;
+                this.RaisePropertyChanged(nameof(this.HigherValue));
+            }
+        }
+
+        public Double MinimumValue
+        {
+            get
+            {
+                return this.minimumValue;
+            }
+
+            set
+            {
+                this.minimumValue = value;
+                this.RaisePropertyChanged(nameof(this.MinimumValue));
+            }
+        }
+
+        public Double MaximumValue
+        {
+            get
+            {
+                return this.maximumValue;
+            }
+
+            set
+            {
+                this.maximumValue = value;
+                this.RaisePropertyChanged(nameof(this.MaximumValue));
+            }
         }
 
         /// <summary>
