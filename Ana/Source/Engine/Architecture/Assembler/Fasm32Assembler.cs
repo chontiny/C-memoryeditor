1a5f18c8ee1ab046d3dcf0d32db6ba66258d8a4b
Zachary Canann <zacharycanann@gmail.com>
2017-01-04 15:00:31 -0800

    Code injection template update, FASM assembler proxy service call fix
    Former-commit-id: 0adde3bc301e987db5c92a09db8a1a9759b702b5

--- Ana/Source/Engine/Architecture/Assembler/Fasm32Assembler.cs
+++ Ana/Source/Engine/Architecture/Assembler/Fasm32Assembler.cs
@@ -5,16 +5,16 @@
     using Utils.Extensions;
 
     /// <summary>
-    /// The Fasm assembler for x86/64
+    /// The Fasm assembler for x86/64.
     /// </summary>
     internal class Fasm32Assembler : IAssembler
     {
         /// <summary>
-        /// Assemble the specified assembly code
+        /// Assemble the specified assembly code.
         /// </summary>
-        /// <param name="isProcess32Bit">Whether or not the assembly is in the context of a 32 bit program</param>
-        /// <param name="assembly">The assembly code</param>
-        /// <returns>An array of bytes containing the assembly code</returns>
+        /// <param name="isProcess32Bit">Whether or not the assembly is in the context of a 32 bit program.</param>
+        /// <param name="assembly">The assembly code.</param>
+        /// <returns>An array of bytes containing the assembly code.</returns>
         public Byte[] Assemble(Boolean isProcess32Bit, String assembly)
         {
             // Assemble and return the code
@@ -22,16 +22,17 @@
         }
 
         /// <summary>
-        /// Assemble the specified assembly code at a base address
+        /// Assemble the specified assembly code at a base address.
         /// </summary>
-        /// <param name="isProcess32Bit">Whether or not the assembly is in the context of a 32 bit program</param>
+        /// <param name="isProcess32Bit">Whether or not the assembly is in the context of a 32 bit program.</param>
         /// <param name="assembly">The assembly code.</param>
-        /// <param name="baseAddress">The address where the code is rebased</param>
+        /// <param name="baseAddress">The address where the code is rebased.</param>
         /// <returns>An array of bytes containing the assembly code.</returns>
         public Byte[] Assemble(Boolean isProcess32Bit, String assembly, IntPtr baseAddress)
         {
             // Call proxy service, which simply passes the asm code to Fasm.net to assemble the instructions
-            return ProxyCommunicator.GetInstance().GetProxyService(isProcess32Bit).Assemble(isProcess32Bit, assembly, baseAddress.ToUInt64());
+            // Note: for assembling instructions, we must always use the 32-bit proxy service to assemble both x86/x64 instructions
+            return ProxyCommunicator.GetInstance().GetProxyService(true).Assemble(isProcess32Bit, assembly, baseAddress.ToUInt64());
         }
     }
     //// End class
