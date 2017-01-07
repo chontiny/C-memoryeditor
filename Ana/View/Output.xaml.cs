b605d843991a17402ef7e019f7359ac3a90ce51a
Zachary Canann <zacharycanann@gmail.com>
2017-01-07 01:34:16 -0800

    Final output view cleanup
    Former-commit-id: 4dc9757f1e5e892297bde25214096529fb37d859

--- Ana/View/Output.xaml.cs
+++ Ana/View/Output.xaml.cs
@@ -4,40 +4,50 @@
     using System.Windows.Controls;
 
     /// <summary>
-    /// Interaction logic for Output.xaml
+    /// Interaction logic for Output.xaml.
     /// </summary>
     internal partial class Output : UserControl
     {
-        private Boolean autoScroll;
-
         /// <summary>
-        /// Initializes a new instance of the <see cref="LabelThresholder" /> class
+        /// Initializes a new instance of the <see cref="Output" /> class.
         /// </summary>
         public Output()
         {
             this.InitializeComponent();
+
+            this.OutputScrollViewer.ScrollToBottom();
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether or not to automatically scroll to bottom.
+        /// </summary>
+        private Boolean AutoScroll { get; set; }
+
+        /// <summary>
+        /// Event that is triggered when the scroll viewer size changes. Will automatically scroll to bottom if already at the bottom.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event args.</param>
         private void OutputScrollViewerScrollChanged(Object sender, ScrollChangedEventArgs e)
         {
-            // User scroll event : set or unset autoscroll mode
+            // User scroll event: set or unset autoscroll mode
             if (e.ExtentHeightChange == 0)
             {
-                // Content unchanged : user scroll event
+                // Content unchanged: user scroll event
                 if (this.OutputScrollViewer.VerticalOffset == this.OutputScrollViewer.ScrollableHeight)
                 {
                     // Scroll bar is in bottom Set autoscroll mode
-                    autoScroll = true;
+                    this.AutoScroll = true;
                 }
                 else
                 {
-                    // Scroll bar isn't in bottomUnset autoscroll mode
-                    autoScroll = false;
+                    // Scroll bar is not in bottomUnset autoscroll mode
+                    this.AutoScroll = false;
                 }
             }
 
-            // Content scroll event : autoscroll eventually
-            if (autoScroll && e.ExtentHeightChange != 0)
+            // Content scroll event: autoscroll eventually
+            if (this.AutoScroll && e.ExtentHeightChange != 0)
             {
                 // Content changed and autoscroll mode set Autoscroll
                 this.OutputScrollViewer.ScrollToVerticalOffset(this.OutputScrollViewer.ExtentHeight);
