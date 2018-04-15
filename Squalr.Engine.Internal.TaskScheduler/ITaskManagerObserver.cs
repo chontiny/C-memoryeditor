05fde7b6c0ae33dd1b353f014290670d6c06ba04
Zachary Canann <zacharycanann@gmail.com>
2018-04-15 11:53:52 -0700

    Fix to action scheduler view model not displaying tasks

---
+++ Squalr.Engine.Internal.TaskScheduler/ITaskManagerObserver.cs
@@ -0,0 +1,15 @@
+﻿namespace Squalr.Engine.TaskScheduler
+{
+    /// <summary>
+    /// An interface for an object that listens for changes in scheduled tasks.
+    /// </summary>
+    public interface ITaskManagerObserver
+    {
+        /// <summary>
+        /// Recieves indication tasks were updated.
+        /// </summary>
+        void OnTaskListChanged();
+    }
+    //// End interface
+}
+//// End namespace
