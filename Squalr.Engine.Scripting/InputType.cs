050e33d585096b27f7e8fb24c2f1ef5acfc2338d
Zachary Canann <zacharycanann@gmail.com>
2018-05-16 22:40:43 -0700

    Adding mod attribute

---
+++ Squalr.Engine.Scripting/InputType.cs
@@ -0,0 +1,19 @@
+﻿namespace Squalr.Engine.Scripting
+{
+    public enum InputType
+    {
+        Button,
+
+        Checkbox,
+
+        RadioButton,
+
+        Number,
+
+        Text,
+
+        Slider,
+    }
+    //// End class
+}
+//// End namespace
